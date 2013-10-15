aa11d6cac66b05ff84612bf46e5f1cc9f5e426f1
radk0s <radk0slaw@gmail.com>
2013-10-15 19:47:37 +0200

    comit

--- Lab1/src/Counter.java
+++ Lab1/src/Counter.java
@@ -5,13 +5,18 @@
  * Time: 19:13
  * To change this template use File | Settings | File Templates.
  */
+
+/*
+    Windows 64 bit okolo 2.7sek, z synch okolo 7 sek
+ */
 public class Counter {
     int count = 0;
 
-    public void inc(){
+
+    public synchronized void inc(){
         ++count;
     }
-    public void dec(){
+    public synchronized void dec(){
         --count;
     }
     public static void main(String... args){
@@ -20,7 +25,7 @@ public class Counter {
 
         new Thread(){
             public void run(){
-                for(int i=0;i<30000;i++)
+                for(int i=0;i<50000000;i++)
                     CounterObj.inc();
                 System.out.println(CounterObj.count);
             }
@@ -28,7 +33,7 @@ public class Counter {
 
         new Thread(){
             public void run(){
-                for(int i=0;i<30000;i++)
+                for(int i=0;i<50000000;i++)
                     CounterObj.dec();
                 System.out.println(CounterObj.count);
             }
