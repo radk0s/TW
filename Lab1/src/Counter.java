c8cb51fa79201254d67e6acb029dc56f34938c38
radk0s <radk0slaw@gmail.com>
2013-10-15 20:19:34 +0200

    comit

--- Lab1/src/Counter.java
+++ Lab1/src/Counter.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Radek
@@ -7,34 +9,37 @@
  */
 
 /*
-    Windows 64 bit okolo 2.7sek, z synch okolo 7 sek
+    Windows 64 bit prawie natychmiast, z synch okolo 41 sek - core i5-3210m, z atomic okolo 32 sek
+    Ubuntu 12.10 OpenJDK 64 bit prawie natychmiast, z synch okolo 45 sek - core2duo p8600
  */
 public class Counter {
-    int count = 0;
+
+    private final AtomicInteger count= new AtomicInteger(0);
+    //int count = 0;
 
 
-    public synchronized void inc(){
-        ++count;
-    }
-    public synchronized void dec(){
-        --count;
-    }
     public static void main(String... args){
 
         final Counter CounterObj = new Counter();
 
         new Thread(){
             public void run(){
-                for(int i=0;i<50000000;i++)
-                    CounterObj.inc();
+                for(int i=0;i<500000000;i++)
+                    CounterObj.count.getAndIncrement();
+                    /*synchronized(CounterObj){
+                        CounterObj.count++;
+                    }*/
                 System.out.println(CounterObj.count);
             }
         }.start();
 
         new Thread(){
             public void run(){
-                for(int i=0;i<50000000;i++)
-                    CounterObj.dec();
+                for(int i=0;i<500000000;i++)
+                    CounterObj.count.getAndDecrement();
+                    /*synchronized(CounterObj){
+                        CounterObj.count--;
+                    }*/
                 System.out.println(CounterObj.count);
             }
         }.start();
